ff10ad176b1c917339511bad737cdd44f2c53973
Drew Franklin <drewfranklin@Drews-MacBook-Pro.local>
2019-03-14 16:23:14 -0400

    added carousel

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -29,3 +29,39 @@ function showSlides(n) {
 function myFunction(x) {
   x.classList.toggle("change");
 }
+
+//nav-barmain open and close
+let toggleNavStatus = false;
+ 
+let toggleNav = function() {
+
+  let getSideBar = document.querySelector("#nav-sideBar"); //document = act on an element  querySelector = grab something
+  let getSideBarUl = document.querySelector(".nav-main ul"); 
+  let getSideBarAnchors = document.querySelectorAll(".nav-main ul a"); //all selects all anchor tags - not just the first one
+
+  if (toggleNavStatus == false) {
+    getSideBarUl.style.visibility = "visible";
+    getSideBar.style.width = "273px";
+    getSideBar.style.opacity = "1";
+
+    let arrayLength = getSideBarAnchors.length;
+    for (let i=0; i < array.length; i++) {
+      getSideBarAnchors[i].style.opacity = "1";   //grab i which is zero - so i = 0, first time loop grab first index, next loop grab next link / index.. so on then style them
+    }
+    toggleNavStatus = true;
+ }
+  
+  if (toggleNavStatus == true) {
+    getSideBarUl.style.visibility = "hidden";
+    getSideBar.style.width = "2.3rem";
+    getSideBar.style.opacity = "1";
+    
+    let arrayLength = getSideBarAnchors.length;
+    for (let i=0; i < array.length; i++) {
+      getSideBarAnchors[i].style.opacity = "1";   //grab i which is zero - so i = 0, first time loop grab first index, next loop grab next link / index.. so on then style them
+    }
+
+
+  }
+
+}
