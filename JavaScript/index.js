885cbe66105901f4e140700e5266cbdd248f4736
Drew Franklin <drewfranklin@drews-mbp.attlocal.net>
2019-03-14 23:01:22 -0400

    fixed small bugs

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -30,37 +30,31 @@ function myFunction(x) {
   x.classList.toggle("change");
 }
 
-//nav-barmain open and close
-let toggleNavStatus = false;
- 
+//nav-barMain open and close
+
 let toggleNav = function() {
+  let toggleNavStatus = false;
 
   let getSideBar = document.querySelector("#nav-sideBar"); //document = act on an element  querySelector = grab something
   let getSideBarUl = document.querySelector(".nav-main ul"); 
-  let getSideBarAnchors = document.querySelectorAll(".nav-main ul a"); //all selects all anchor tags - not just the first one
 
-  if (toggleNavStatus == false) {
+  if (!toggleNavStatus) {
     getSideBarUl.style.visibility = "visible";
     getSideBar.style.width = "273px";
-    getSideBar.style.opacity = "1";
 
     let arrayLength = getSideBarAnchors.length;
     for (let i=0; i < array.length; i++) {
       getSideBarAnchors[i].style.opacity = "1";   //grab i which is zero - so i = 0, first time loop grab first index, next loop grab next link / index.. so on then style them
     }
     toggleNavStatus = true;
- }
-  
-  if (toggleNavStatus == true) {
+  } else {
     getSideBarUl.style.visibility = "hidden";
-    getSideBar.style.width = "2.3rem";
-    getSideBar.style.opacity = "1";
+    getSideBar.style.width = "36px";
     
     let arrayLength = getSideBarAnchors.length;
     for (let i=0; i < array.length; i++) {
       getSideBarAnchors[i].style.opacity = "1";   //grab i which is zero - so i = 0, first time loop grab first index, next loop grab next link / index.. so on then style them
     }
-
 
   }
 
